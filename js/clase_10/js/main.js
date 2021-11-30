1b7a387f0bcfb0b8754f9dd91975b8801766e83d
ianRamirez <ian9ofi32@gmail.com>
2021-11-29 23:01:03 -0600

    Clase 10 Falta algo de JS

---
+++ js/clase_10/js/main.js
@@ -0,0 +1,11 @@
+// document.getElementsByTagName('button')[0].addEventListener('click', mensaje);
+// function mensaje() {
+//   console.log('hee hee ');
+//   console.log('hee hee ');
+// }
+
+document.getElementsByTagName('button')[0].addEventListener('click', mensaje);
+document.getElementsByTagName('button')[1].addEventListener('click', mensaje);
+function mensaje(event) {
+  event.target.s;
+}
