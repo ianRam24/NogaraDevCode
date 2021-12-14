406df234ef5b620c5fe31d885c7252701c6a20c8
ianRamirez <ian9ofi32@gmail.com>
2021-12-13 19:09:59 -0600

    Agregando la clase 12 Le falta el diseño

---
+++ js/clase_12/js/main.js
@@ -0,0 +1,27 @@
+var urlApi = 'https://rickandmortyapi.com/api/character';
+fetch(urlApi)
+  .then((response) => response.json())
+  .then(function (json) {
+    var container = document.getElementById('container');
+    json.results.map(function (results) {
+      container.innerHTML +=
+        `
+      <div> <img src= ` +
+        results.image +
+        `></div>
+      <strong class="card-text">` +
+        results.name +
+        `</strong><br>
+      <span class="card-text">` +
+        results.species +
+        `</span><br>
+      <i class="card-text">` +
+        results.status +
+        `</i><br>
+      <hr>
+      
+      `;
+    });
+  });
+/*  .then((response) => response.json()) Se convierte en JSON
+ .then((data) => console.log(data));  JSON cambia a data y se muestra por consola}*/
