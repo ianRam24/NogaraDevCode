748883ecb96252371960a30fbcc35299c6ea10f0
ianRamirez <ian9ofi32@gmail.com>
2021-12-20 22:05:00 -0600

    clase 15 con el this en objetos

---
+++ js/clase_15/js/main.js
@@ -0,0 +1,26 @@
+let batman = {
+  nombre: 'Bruce',
+  apellido: 'Wayne',
+  edad: 35,
+  estatus: 'Millonario',
+};
+
+// const superman = (name, age, status) => {
+//   let element = {
+//     name: name,
+//     age: age,
+//     status: status,
+//   };
+//   console.log(element);
+// };
+
+// superman('Clark', 34, 'Writter');
+
+function superHeroe(name, age, estatus) {
+  this.name = name;
+  this.age = age;
+  this.estatus = estatus;
+}
+let superman = new superHeroe('Clark', 36, 'writter');
+
+// this.value
