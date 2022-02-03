d09190ba291aaad3ba450c24df3ec788a14a22ad
ianRamirez <ian9ofi32@gmail.com>
2022-02-03 00:49:20 -0600

    Clase 17

---
+++ js/clase_17/js/main.js
@@ -0,0 +1,22 @@
+let wWindow = window.innerWidth;
+let hWindow = window.innerHeight;
+
+const btn = document.getElementById('pop-up');
+function geoLocalitation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    alert('No esta soportado por el navegador');
+  }
+  console.log('You have been hacked');
+}
+
+function showPosition(position) {
+  console.log(position);
+}
+btn.addEventListener('click', geoLocalitation());
+
+/* screen.orientation.type=> para saber la orientacion del telefono: landscape-primary: Horizontal y portrait-primary: Vertical */
+// screen.width => Saca el Ancho de laa pantalla
+// screen.height=> Saca el alto de la pantalla
+// 19.5584975 / -99.1440035 Caress
